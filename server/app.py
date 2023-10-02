6a75054c24e767496f9caaefa58a34a711e5c2da
misra2023 <mishazabdi@gmail.com>
2023-10-02 11:20:40 +0300

    completed assignment

--- server/app.py
+++ server/app.py
@@ -1,30 +1,54 @@
-from flask import Flask, request, session, jsonify, make_response
+from flask import Flask, request, session, jsonify, make_response, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.json.compact = False
 
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
-@app.route('/sessions/<string:key>', methods=['GET'])
-def show_session(key):
+# Initialize Flask-Login
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-    session["hello"] = session.get("hello") or "World"
-    session["goodnight"] = session.get("goodnight") or "Moon"
 
-    response = make_response(jsonify({
-        'session': {
-            'session_key': key,
-            'session_value': session[key],
-            'session_accessed': session.accessed,
-        },
-        'cookies': [{cookie: request.cookies[cookie]}
-            for cookie in request.cookies],
-    }), 200)
+# Dummy User Model (Replace with your actual user model if using a database)
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
 
-    response.set_cookie('mouse', 'Cookie')
 
-    return response
+# Dummy user data (Replace with your user data)
+users = {'user1': 'password1', 'user2': 'password2'}
+
+# Login route
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if username in users and users[username] == password:
+        user = User(username)
+        login_user(user)
+        return redirect(url_for('dashboard'))
+    else:
+        return 'Login failed. Invalid credentials.'
+
+
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return 'Logged out successfully.'
+
+
+# Dashboard route (requires login)
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return f'Welcome, {current_user.id}! You are logged in.'
+
 
 if __name__ == '__main__':
     app.run(port=5555)
-    
